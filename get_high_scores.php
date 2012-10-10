3f6ba7c220015515407a7d132b9f3ebf18016f1e
U-Vortex\Sreeram <Sreeram@Vortex.(none)>
2012-10-10 02:49:46 -0500

    updated get_high_scores to use json encoding

--- get_high_scores.php
+++ get_high_scores.php
@@ -13,12 +13,19 @@
   $dbs = mysql_select_db($database, $con);
 
   //Query database for data
-  $result = mysql_query("SELECT * FROM $tableName order by score desc LIMIT 15");          //query
-  
-  //TODO: Need to scrub the results of everything but name and score
+  $result = mysql_query("SELECT first_name,last_name,score FROM $tableName order by score desc LIMIT 15");          //query
+
+  $parsed_results = array();
+  while ($row = mysql_fetch_array($result)) {
+    $parsed_results[] = array( 
+      'first_name' => $row['first_name'],
+      'last_name' => $row['last_name'],
+      'score' => $row['score'],
+    );
+  } 
   //$array = mysql_fetch_row($result);                          //fetch result    
 
   //echo result as json 
-  //echo json_encode($array);
+  echo json_encode($parsed_results);
 
 ?>
