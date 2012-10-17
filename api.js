a38c52d195c1337f15309fa156ca8c89cd129504
U-Vortex\Sreeram <Sreeram@Vortex.(none)>
2012-10-17 01:42:11 -0500

    updated api.js

--- api.js
+++ api.js
@@ -1,33 +1,37 @@
 //this is where we write the AJAX and JSON type requests and POSTS to server to store data
+var xmldebug = new XMLHttpRequest();
 
 function sendUserInfo() {
- //retrive high scores and store in highScores array
-  var playerInfoEncoded = JSON.stringify(playerInfo);
+  //$.post("add_user.php",{JSON.stringify(playerInfo)});
+  var playerInfoEncoded = "jsonUserInfo=" + JSON.stringify(playerInfo);
   if (window.XMLHttpRequest) {
     xmlhttp=new XMLHttpRequest();
     xmlhttp.open("POST","add_user.php",true);
     xmlhttp.send(playerInfoEncoded);
-    //$.post("add_user.php",{JSON.stringify(playerInfo)});
   }
 } 
 
 function retrieveHighScores() {
   if (window.XMLHttpRequest) {   // code for IE7+, Firefox, Chrome, Opera, Safari
     xmlhttp=new XMLHttpRequest();
+    xmldebug = xmlhttp;
   }
   else { // code for IE6, IE5
     xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
   } 
   xmlhttp.onreadystatechange=function() {
     if (xmlhttp.readyState==4 && xmlhttp.status==200) {
-    //document.getElementById("txtHint").innerHTML=xmlhttp.responseText;
+      //document.getElementById("txtHint").innerHTML=xmlhttp.responseText;
+      $("getuserinfotxt").innerHTML = xmlhttp.responseText;
+      alert(xmlhttp.responseText);
+      output = eval("(" + xmlhttp.responseText + ")");
+      //output = JSON.parse(xmlhttp.responseText,reviver);      //i think this needs the json2 JS file
       for (var i=0;i<15;i++) {
-        highScores[i].name = xmlhttp.responseText ;
-        highScores[i].score = xmlhttp.responseText ;
+        highScores[i].name = output[i].first_name + output[i].last_name;
+        highScores[i].score = output[i].score;
       }
     }
   }
   xmlhttp.open("GET","get_high_scores.php",true);
-  //xmlhttp.open("GET","get_high_scores.php?q="+str,true);
   xmlhttp.send();
 } 
