793063efac925f8654fdea6b028c5a9843dc06b9
U-Vortex\Sreeram <Sreeram@Vortex.(none)>
2012-10-17 01:28:00 -0500

    fixed some things in add_user.php

--- add_user.php
+++ add_user.php
@@ -9,22 +9,22 @@
   $database = "enovagames";
   $tableName = "quickkick";
 
-  if(isset($_POST["playerInfo"])){
-    $playerInfo_json = $_POST["playerInfo"];
-    $JSONArray  = json_decode($playerInfo_json, true); //returns null if not decoded
+  if(isset($_POST["jsonUserInfo"])){
+    $JSONArray  = json_decode($_POST["jsonUserInfo"],true); //returns null if not decoded
     //Values can now be accessed like standard PHP array
     if($JSONArray !== null){ 
-        $firstname = $JSONArray["firstname"];
-        $lastname = $JSONArray["lastname"];
-        $email = $JSONArray["email"];
-        $score = $JSONArray["score"];
-        $agree_to_terms = $JSONArray["agree_to_terms"];
+      $firstname = $JSONArray["firstname"];
+      $lastname = $JSONArray["lastname"];
+      $email = $JSONArray["email"];
+      $score = $JSONArray["score"];
+      $agree_to_terms = $JSONArray["agree_to_terms"];
+  
+      //Connect to mysql database
+      $con = mysql_connect($host,$user,$pass);
+      $dbs = mysql_select_db($database, $con);
+
+      //Query database for data
+      $result = mysql_query("INSERT into $database.$tableName (first_name,last_name,email,score,agree_to_terms) values ($firstname,$lastname,$email,$score,$agree_to_terms)"); 
     }
   } 
-  //Connect to mysql database
-  $con = mysql_connect($host,$user,$pass);
-  $dbs = mysql_select_db($database, $con);
-
-  //Query database for data
-  $result = mysql_query("INSERT into $database.$tableName (first_name,last_name,email,score,agree_to_terms) values ($firstname,$lastname,$email,$score,$agree_to_terms)"); 
 ?>
